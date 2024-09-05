c43debf6ddda3aa73f71683bd1cd406a711dd9c4
kaique-oliveira-sete <dev5setedigital@gmail.com>
2024-09-05 15:03:44 -0300

    ajuste do vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,4 +5,12 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    host: '0.0.0.0', // Permite acesso externo
+    port: 3004, // Define a porta que o servidor irá escutar
+  },
+  preview: {
+    host: '0.0.0.0', // Permite acesso externo no modo preview
+    port: 3004, // Define a porta que o servidor irá escutar no modo preview
+  },
 });
